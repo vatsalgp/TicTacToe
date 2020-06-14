a0ea3c87ee5d01600f8db9a882ce54307ea3a195
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-14 23:36:16 +0530

    refactored

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,33 +1,41 @@
-/*
-    Rows and Cols start from 1.
-    X starts first.
-    X -> 1
-    O -> -1
-*/
-
 import java.util.Scanner;
 
 public class TicTacToe {
-    private static int[][] matrix = new int[3][3];
-    private static int move = 1;
-    private static State state = State.Empty;
+    private static Position[][] matrix = new Position[3][3];
+    private static Position move = Position.X;
+    private static GameState state = GameState.OnGoing;
     final private static Scanner scanner = new Scanner(System.in);
 
-    private static enum State {
-        X("X wins"), O("O wins"), Draw("Draw"), Empty("Game not finished"), Impossible("Impossible");
+    private static enum Position {
+        Empty(" "), X("X"), O("O");
 
-        String value;
+        private String value;
 
-        State(String val) {
-            value = val;
+        Position(String value) {
+            this.value = value;
         }
 
-        String getValue() {
+        public String getValue() {
+            return value;
+        }
+    }
+
+    private static enum GameState {
+        X("X wins"), O("O wins"), Draw("Draw"), OnGoing("Game not finished"), Impossible("Impossible");
+
+        private String value;
+
+        GameState(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
             return value;
         }
     }
 
     public static void main(String[] args) {
+        initMatrix();
         while (!hasGameEnded()) {
             printMatrix();
             updateMatrix();
@@ -37,22 +45,33 @@ public class TicTacToe {
         System.out.println(state.getValue());
     }
 
+    private static void initMatrix() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                matrix[i][j] = Position.Empty;
+    }
+
     private static boolean hasGameEnded() {
-        return state == State.X || state == State.O || state == State.Draw;
+        return state == GameState.X || state == GameState.O || state == GameState.Draw;
     }
 
     private static void updateMatrix() {
+        if (move == Position.X)
+            System.out.println("X's Turn");
+        else
+            System.out.println("O's Turn");
         while (true) {
-            System.out.print("Enter the coordinates: ");
+            System.out.print("Enter the row number(1, 2, 3): ");
             int x = scanner.nextInt() - 1;
+            System.out.print("Enter the col number(1, 2, 3): ");
             int y = scanner.nextInt() - 1;
             if (!(x >= 0 && x <= 2 && y >= 0 && y <= 2))
                 System.out.println("Invalid cell");
-            else if (matrix[x][y] != 0)
+            else if (matrix[x][y] != Position.Empty)
                 System.out.println("This cell is occupied! Choose another one!");
             else {
                 matrix[x][y] = move;
-                move *= -1;
+                move = move == Position.X ? Position.O : Position.X;
                 return;
             }
         }
@@ -60,22 +79,10 @@ public class TicTacToe {
 
     private static void printMatrix() {
         System.out.println("---------");
-        for (int[] arr : matrix) {
+        for (Position[] row : matrix) {
             System.out.print("| ");
-            for (int ele : arr) {
-                String out = "";
-                switch (ele) {
-                    case 1:
-                        out = "X ";
-                        break;
-                    case -1:
-                        out = "O ";
-                        break;
-                    default:
-                        out = "  ";
-                }
-                System.out.print(out);
-            }
+            for (Position pos : row)
+                System.out.print(pos.getValue() + " ");
             System.out.println("|");
         }
         System.out.println("---------");
@@ -83,61 +90,61 @@ public class TicTacToe {
 
     private static void updateState() {
         if (checkForWinForX() && checkForWinForO() || Math.abs(countX() - countO()) > 1)
-            state = State.Impossible;
+            state = GameState.Impossible;
         else if (checkForWinForX())
-            state = State.X;
+            state = GameState.X;
         else if (checkForWinForO())
-            state = State.O;
+            state = GameState.O;
         else if (hasEmptyCells())
-            state = State.Empty;
+            state = GameState.OnGoing;
         else
-            state = State.Draw;
+            state = GameState.Draw;
     }
 
-    private static boolean checkForWin(int c) {
+    private static boolean checkForWin(Position pos) {
         for (int i = 0; i < 3; i++)
-            if (matrix[i][0] == c && matrix[i][1] == c && matrix[i][2] == c)
+            if (matrix[i][0] == pos && matrix[i][1] == pos && matrix[i][2] == pos)
                 return true;
         for (int i = 0; i < 3; i++)
-            if (matrix[0][i] == c && matrix[1][i] == c && matrix[2][i] == c)
+            if (matrix[0][i] == pos && matrix[1][i] == pos && matrix[2][i] == pos)
                 return true;
-        if (matrix[0][0] == c && matrix[1][1] == c && matrix[2][2] == c)
+        if (matrix[0][0] == pos && matrix[1][1] == pos && matrix[2][2] == pos)
             return true;
-        if (matrix[0][2] == c && matrix[1][1] == c && matrix[2][0] == c)
+        if (matrix[0][2] == pos && matrix[1][1] == pos && matrix[2][0] == pos)
             return true;
         return false;
     }
 
     private static boolean checkForWinForX() {
-        return checkForWin(1);
+        return checkForWin(Position.X);
     }
 
     private static boolean checkForWinForO() {
-        return checkForWin(-1);
+        return checkForWin(Position.O);
     }
 
     private static boolean hasEmptyCells() {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                if (matrix[i][j] == 0)
+        for (Position[] row : matrix)
+            for (Position pos : row)
+                if (pos == Position.Empty)
                     return true;
         return false;
     }
 
-    private static int count(int c) {
+    private static int count(Position givenPos) {
         int out = 0;
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                if (matrix[i][j] == c)
+        for (Position[] row : matrix)
+            for (Position pos : row)
+                if (pos == givenPos)
                     out++;
         return out;
     }
 
     private static int countX() {
-        return count(1);
+        return count(Position.X);
     }
 
     private static int countO() {
-        return count(-1);
+        return count(Position.O);
     }
 }
